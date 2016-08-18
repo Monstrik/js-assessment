fd73012321c3469b0ca04c6359d8d61eb67a6038
Alexander <coolspammail@gmail.com>
2016-08-18 02:39:34 -0400

    fix async.js And objects.js

--- app/objects.js
+++ app/objects.js
@@ -1,15 +1,24 @@
 exports = typeof window === 'undefined' ? global : window;
 
+
 exports.objectsAnswers = {
-  alterContext: function(fn, obj) {
+    alterContext: function (fn, obj) {
+        return fn.call(obj);
+    },
 
-  },
+    alterObjects: function (constructor, greeting) {
+        constructor.prototype.greeting = greeting;
+    },
 
-  alterObjects: function(constructor, greeting) {
+    iterate: function (obj) {
+        var ret = [];
 
-  },
+        for (var prop in obj) {
+            if (obj.hasOwnProperty(prop)) {
+                ret.push(prop + ': ' + obj[prop]);
+            }
+        }
 
-  iterate: function(obj) {
-
-  }
+        return ret;
+    }
 };
